31d1d51c12f93e0c1276f60bc6d6d67f0a62ab8a
tosyama <tos-yama@sa2.so-net.ne.jp>
2017-04-05 07:21:55 +0000

    Add test cases.

--- test/shogiruleTest.cpp
+++ test/shogiruleTest.cpp
@@ -1,9 +1,25 @@
 #include "catch.hpp"
+#include <stdio.h>
 #include "../shogiban.h"
+#include "../kyokumencode.h"
+#include "../sashite.h"
+#include "../shogirule.h"
 
 TEST_CASE("Create all sashite.", "[rule]")
 {
     ShogiKykumen shogi;
     resetShogiBan(&shogi);
-	REQUIRE(1);
+    Sashite s[600];
+    int n;
+	
+	// first case
+    createSashiteAll(&shogi, s, &n);
+	CHECK(n==30);
+
+	// max case
+	char *code = ".43~~~~00/02~0-~0;~30~~~~00uwy~H000~~~~~~~~~~~~~~~~~~";
+	loadKyokumenFromCode(&shogi, code);
+    createSashiteAll(&shogi, s, &n);
+	CHECK(n==593);
+
 }
